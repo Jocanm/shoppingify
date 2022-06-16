288792cc908f1e4f6440309f8d21cb98e5454ca4
Jose Angarita <amjose09@gmail.com>
2022-06-16 13:01:16 -0500

    test

--- src/components/shopping_list/ShoppingList.styles.ts
+++ src/components/shopping_list/ShoppingList.styles.ts
@@ -10,7 +10,7 @@ export const ShoppingListContainer = styled.section<Props>`
     ${NoVisivleScrollBar}
 
     width: 100%;
-    height: 100%;
+    height: 100vh;
     overflow-y: auto;
 
     display: flex;
