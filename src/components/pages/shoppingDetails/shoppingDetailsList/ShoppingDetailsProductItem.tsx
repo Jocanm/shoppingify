b305fb714fa089f9601a6bd263b78f30e89806be
Jose Angarita <amjose09@gmail.com>
2022-07-07 09:23:53 -0500

    [UPDATED] funcion para eliminar producto migrada a rtk query

--- src/components/pages/shoppingDetails/shoppingDetailsList/ShoppingDetailsProductItem.tsx
+++ src/components/pages/shoppingDetails/shoppingDetailsList/ShoppingDetailsProductItem.tsx
@@ -12,14 +12,11 @@ const variants = {
         opacity: 1,
         x: 0,
         transition: {
-            // delay: (i + 0.3) * 0.2,
-            delay: (i + 0.2) * 0.1,
-            duration: 0.3,
-            // duration: 0.5,
+            delay: (i + 0.1) * 0.1,
+            duration: 0.4,
         }
     }),
     hidden: { opacity: 0, x: 100 },
-    // hidden: { opacity: 0 },
 }
 
 export const ShoppingDetailsProductItem = ({ purchasedProduct, position }: Props) => {
